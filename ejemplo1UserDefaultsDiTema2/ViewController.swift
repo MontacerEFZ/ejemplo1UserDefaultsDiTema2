21c0dfc8bfd7b2442d025c0807802a0876161121
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-11-30 09:17:58 +0100

    Initial Commit

---
+++ ejemplo1UserDefaultsDiTema2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejemplo1UserDefaultsDiTema2
+//
+//  Created by Montacer El Fazazi on 30/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ejemplo1UserDefaultsDiTema2Tests/ejemplo1UserDefaultsDiTema2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ejemplo1UserDefaultsDiTema2Tests.swift
+//  ejemplo1UserDefaultsDiTema2Tests
+//
+//  Created by Montacer El Fazazi on 30/11/2023.
+//
+
+import XCTest
+@testable import ejemplo1UserDefaultsDiTema2
+
+class ejemplo1UserDefaultsDiTema2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
